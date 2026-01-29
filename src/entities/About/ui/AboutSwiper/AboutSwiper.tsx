5d39cb2d178aa077e3b92c8812d3d7bca0e9405e
Bekbol <bekbol.bv@gmail.com>
2026-01-29 15:29:14 +0500

    about swiper navigate

--- src/entities/About/ui/AboutSwiper/AboutSwiper.tsx
+++ src/entities/About/ui/AboutSwiper/AboutSwiper.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import Img1 from "@/shared/assets/about/swiper/1.png";
 import Img2 from "@/shared/assets/about/swiper/2.png";
 import Img3 from "@/shared/assets/about/swiper/3.png";
-
-import { Autoplay } from "swiper/modules";
-
+import { Navigation } from "swiper/modules";
 import styles from "./AboutSwiper.module.scss";
+import RightIcon from "@/shared/assets/icons/arrow-right-long-line.svg";
 
 const items = [
   { id: 1, img: Img1 },
@@ -24,27 +23,46 @@ const items = [
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/navigation";
+import { Button } from "@/shared/ui/Button";
+import { useCallback, useRef } from "react";
 
 function AboutSwiper() {
+  const sliderRef = useRef<SwiperRef>(null);
+
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev();
+  }, []);
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+  }, []);
+
   return (
     <Swiper
+      ref={sliderRef}
       slidesPerView={1}
       spaceBetween={10}
       pagination={{
         clickable: true,
       }}
-      modules={[Autoplay]}
+      modules={[Navigation]}
       autoplay={{
         delay: 2500,
         disableOnInteraction: false,
       }}
-      loop={true}
-      className={styles.swiperWrapper}
+      navigation={{
+        prevEl: styles.btnLeft,
+        nextEl: styles.btnRight,
+      }}
       breakpoints={{
         768: {
           slidesPerView: 3,
         },
       }}
+      className={styles.swiperWrapper}
     >
       {items.map((el) => (
         <SwiperSlide key={el.id} className={styles.swiperItem}>
@@ -57,6 +75,23 @@ function AboutSwiper() {
           />
         </SwiperSlide>
       ))}
+
+      <div className={styles.btns}>
+        <Button
+          variyant="secondary"
+          className={styles.btnLeft}
+          onClick={handlePrev}
+        >
+          <Image src={RightIcon} alt="about" width={20} height={20} />
+        </Button>
+        <Button
+          variyant="secondary"
+          className={styles.btnRight}
+          onClick={handleNext}
+        >
+          <Image src={RightIcon} alt="about" width={20} height={20} />
+        </Button>
+      </div>
     </Swiper>
   );
 }
