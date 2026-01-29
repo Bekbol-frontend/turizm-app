e216be5d61436f72f0f465670d498eef35e8a80c
Bekbol <bekbol.bv@gmail.com>
2026-01-29 17:49:57 +0500

    usehooks-ts added

--- src/shared/lib/useResponsive/index.ts
+++ src/shared/lib/useResponsive/index.ts
@@ -1,25 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
-export function useResponsive(breakpoint: number = 850) {
-  const [mobile, setMobile] = useState(false);
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia(`(max-width: ${breakpoint}px)`);
-
-    setMobile(mediaQuery.matches);
-
-    const handleChange = (e: MediaQueryListEvent) => {
-      setMobile(e.matches);
-    };
-
-    mediaQuery.addEventListener("change", handleChange);
-
-    return () => {
-      mediaQuery.removeEventListener("change", handleChange);
-    };
-  }, [breakpoint]);
+export function useResponsive() {
+  const mobile = useMediaQuery("(max-width: 850px)");
 
   return { mobile };
 }
