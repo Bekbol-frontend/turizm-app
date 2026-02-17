39edad00af61d42b40993a4908bffc4d0747cd2f
Bekbol <bekbol.bv@gmail.com>
2026-02-17 16:31:03 +0500

    fix layout graph image;

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -72,8 +72,8 @@ export async function generateMetadata({
       type: "website",
       images: [
         {
-          url: "/logo.svg",
-          width: 630,
+          url: "/catalog/bg.jpg",
+          width: 1200,
           height: 630,
           alt: "Aral Sea Tour Uzbekistan | Qaraqalpaqstan",
         },
@@ -84,7 +84,7 @@ export async function generateMetadata({
       card: "summary_large_image",
       title: t.title,
       description: t.description,
-      images: ["/logo.svg"],
+      images: ["/catalog/bg.jpg"],
     },
   };
 }
